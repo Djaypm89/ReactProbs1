801fe40901957e042f448af99cd087f4f2043541
Djaypm89 <djaypm89@gmail.com>
2021-09-09 18:16:40 -0400

    part 3 cleaned up

--- src/components/AlertButton/AlertButton.jsx
+++ src/components/AlertButton/AlertButton.jsx
@@ -14,19 +14,6 @@ const AlertButton = (props) => {
     );      
 }
 
-// const DisplayNames = (props) => {
-//     return ( 
-//         <React.Fragment>
-//             <h2>Names List</h2>
-//             <hr />
-//             <ul>
-//                 {props.listedNames.map((name) => {
-//                     return <li>{name}</li>
-//                 })}
-//             </ul>
-//         </React.Fragment>
-//      );
-// }
  
  
 export default AlertButton;
